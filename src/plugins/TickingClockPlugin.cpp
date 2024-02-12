13e57cd6acb4930bcae582df4c12efc61a5b6f54
Martin <xc@gugudada.ch>
2024-02-12 12:34:52 +0100

    more syncs with orig repo

--- src/plugins/TickingClockPlugin.cpp
+++ src/plugins/TickingClockPlugin.cpp
@@ -26,16 +26,15 @@ void TickingClockPlugin::loop()
 
       Screen.clear();
 
-      Screen.drawCharacter(2, 0, Screen.readBytes(system6x7[hh[0] + 48]), 8, Screen.getCurrentBrightness());
-      Screen.drawCharacter(9, 0, Screen.readBytes(system6x7[hh[1] + 48]), 8, Screen.getCurrentBrightness());
-      Screen.drawCharacter(2, 9, Screen.readBytes(system6x7[mm[0] + 48]), 8, Screen.getCurrentBrightness());
-      Screen.drawCharacter(9, 9, Screen.readBytes(system6x7[mm[1] + 48]), 8, Screen.getCurrentBrightness());
+      Screen.drawCharacter(2, 0, Screen.readBytes(fonts[1].data[hh[0]]), 8, Screen.getCurrentBrightness());
+      Screen.drawCharacter(9, 0, Screen.readBytes(fonts[1].data[hh[1]]), 8, Screen.getCurrentBrightness());
+      Screen.drawCharacter(2, 9, Screen.readBytes(fonts[1].data[mm[0]]), 8, Screen.getCurrentBrightness());
+      Screen.drawCharacter(9, 9, Screen.readBytes(fonts[1].data[mm[1]]), 8, Screen.getCurrentBrightness());
       previousMinutes = timeinfo.tm_min;
       previousHour = timeinfo.tm_hour;
     }
     if (previousSecond != timeinfo.tm_sec)
     {
-
       // clear second lane
       Screen.drawRectangle(0, 7, 16, 2, true, 0);
       // alternating second pixel
